936045071569438eca9dc5af2f5111bdf3732e16
yangxin <yangxinyx@boe.com.cn>
2019-06-25 16:22:35 +0800

    init

---
+++ src/main/java/cn/com/boe/b5/fwp/lo/LeanOfficeBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.com.boe.b5.fwp.lo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanOfficeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanOfficeBackendApplication.class, args);
+	}
+
+}
